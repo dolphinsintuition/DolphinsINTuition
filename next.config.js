7bf59e93dbafe8651b2020db4b068ef3c3daa909
dolphinsintuition <dolphinsintuition@gmail.com>
2026-08-03 00:05:49 +0800

    feat: Update Next.js config for MDX support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  trailingSlash: true,
-}
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  experimental: {
+    mdxRs: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
